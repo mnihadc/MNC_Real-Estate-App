78c14a7e65ea3366196ede112cebe4a3f361620b
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-10-14 20:25:20 +0530

    models user created

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-.then(() => {
-  console.log('Connected to MongoDB!');
-})
-.catch((err) => {
-  console.log(err);
-})
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log("error  in connection to mongodb", err);
+  })
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  console.log('Server is running on port 3000!');
 })
--- bhrooms/src/Pages/About.jsx
+++ bhrooms/src/Pages/About.jsx
@@ -4,6 +4,7 @@ function About() {
   return (
     <div>
         about 
+        hjgffjhlhfflgkjhdtyf
     </div>
   )
 }
