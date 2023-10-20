28fce3957b2f9dc10783d6367e6a1e4315c5b3ef
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-10-20 16:42:14 +0530

    create update user api route
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -15,6 +15,9 @@ mongoose.connect(process.env.MONGO)
   })
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.use("/api/user", userRouter);
 app.use('/api/auth', authRouter);
 
@@ -29,7 +32,7 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   })
