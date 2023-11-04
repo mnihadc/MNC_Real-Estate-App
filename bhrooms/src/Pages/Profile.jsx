53628ae3db68b39354ffa0b5df9f7749f9465dad
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-11-04 16:41:49 +0530

    completed show user listings functionality
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- bhrooms/src/Pages/Profile.jsx
+++ bhrooms/src/Pages/Profile.jsx
@@ -17,7 +17,10 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
+
   console.log(formData);
   /*firebase storage
    allow read;
@@ -108,7 +111,20 @@ function Profile() {
       dispatch(signInFailure(error.message));
     }
   }
-
+  const handleShowListings = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -142,7 +158,28 @@ function Profile() {
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign-Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
-      <p className='green-red-700 mt-5'>{updateSuccess ? 'User is updated successfuly' : ''}</p>
+      <p className='bg-red-700 mt-5'>{updateSuccess ? 'User is updated successfuly' : ''}</p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingError ? 'Error show listings' : ''}</p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain' />
+              </Link>
+              <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold flex-1 hover:underline truncate'>
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col items-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
     </div>
   )
 }
