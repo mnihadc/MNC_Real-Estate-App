1f8840bdb866c4b36443ed0547fde553ccb55c02
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-12-02 13:06:19 +0530

    added pre installation
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- api/index.js
+++ api/index.js
@@ -14,19 +14,24 @@ mongoose.connect(process.env.MONGO)
   .catch((err) => {
     console.log("error  in connection to mongodb", err);
   })
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-
-app.use("/api/user", userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing',listingRouter);
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 })
 
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/bhrooms/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bhrooms', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
